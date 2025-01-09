acb3f56d3e46a6cc5a6b3a3c8eba7715ab0cb34a
semengrig <dxerus@yandex.ru>
2025-01-09 12:15:37 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+user_name = "Елена"
+title = "Отработка лида"
+content = "Необходимо совершить звонок клиенту и провести консультацию"
+status = "В работе"
+created_date = "09-01-2025"
+issue_date = "09-01-2025"
+
+print("Имя пользователя:", user_name)
+print("Заголовок заметки:", title)
+print("Описание:", content)
+print("Текущий статус:", status)
+print("Дата создания:", created_date)
+print("Дата окончания:", issue_date)
