f8443dbfef8daa03fcab21e133419f6be89537e6
semengrig <dxerus@yandex.ru>
2025-01-10 14:50:22 +0700

    final.py

--- final.py
+++ final.py
@@ -5,8 +5,8 @@ title3 = input("Введите третий загловок заметки: ")
 titles = [title1, title2, title3]
 content = input("Введите описание заметки: ")
 status = input("Введите текущий статус: ")
-created_date = input("Дата создания: ")
-issue_date = input("Дата выполнения: ")
+created_date = input("Дата создания (в формате DD-MM-YY): ")
+issue_date = input("Дата выполнения (в формате DD-MM-YY): ")
 note = [user_name, content, status, created_date, issue_date, titles]
 
 print(note)
