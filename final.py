fd8d52bbfd3500c628ebfa719de03d687edd7b4c
semengrig <dxerus@yandex.ru>
2025-01-11 09:47:33 +0700

    Update final.py

--- final.py
+++ final.py
@@ -9,4 +9,4 @@ created_date = input("Дата создания (в формате DD-MM-YY): ")
 issue_date = input("Дата выполнения (в формате DD-MM-YY): ")
 note = [user_name, content, status, created_date, issue_date, titles]
 
-print(note)
+print("Данные о заметке: ", "\nИмя пользователя: ", note [0], "\nОписание заметки: ", note [1], "\nТекущий статус: ", note [2], "\nДата создания: ", note [3], "\nДата выполнения: ", note [4], "\nЗаголовки заметки: ", note [5])
